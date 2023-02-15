c7254654e819109e599de311bf735183f60f90d9
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-15 09:25:27 +0530

    [Amol] :Adding New Contact In Address Book

---
+++ AddressBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook.CreateContact();
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("-----------------------------");
+            addressBook.Display();
+            Console.ReadLine();
+        }
+    }
+}
